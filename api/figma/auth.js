a0f72b3468a91fcb6c053e861d21ef411eaf0334
heyjace <jfox@atlassian.com>
2025-12-03 15:14:26 -0800

    Update auth.js

--- api/figma/auth.js
+++ api/figma/auth.js
@@ -1,6 +1,6 @@
 const { Pool, neonConfig } = require('@neondatabase/serverless');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 neonConfig.fetchConnectionCache = true;
 
